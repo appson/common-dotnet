b7b26fc3d4e75de550c52bbe1e6fcc36dbcf7a87
Hamed Iravanchi <iravanchi@gmail.com>
2016-12-10 02:08:00 +0330

    Added the solution and empty project for threading

---
+++ Appson.Common/Threading/Throttler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appson.Common.Threading
+{
+    public class Throttler
+    {
+        public Throttler(int delayMs)
+        {
+            DelayMs = delayMs;
+        }
+
+        public int DelayMs { get; private set; }
+    }
+}
