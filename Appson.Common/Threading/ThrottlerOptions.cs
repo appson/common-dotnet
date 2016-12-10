798ec63efdfb00b4cdd7491a2a8dc89e85c37380
Hamed Iravanchi <iravanchi@gmail.com>
2016-12-10 10:15:50 +0330

    Added ThrottlerOptions empty class

---
+++ Appson.Common/Threading/ThrottlerOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Appson.Common.Threading
+{
+    public class ThrottlerOptions
+    {
+        public int DelayMillis { get; set; } = 100;
+
+        public float RatePerSecond { get; set; } = 20;
+
+        public int MaxBurstSize { get; set; } = 10;
+
+        public int InitialQuota { get; set; } = 0;
+    }
+}
