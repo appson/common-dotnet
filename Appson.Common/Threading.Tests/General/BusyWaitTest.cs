055d36a42c6a3fd096631ed41a9c651f09d6ff7b
Hamed Iravanchi <iravanchi@gmail.com>
2016-12-23 04:17:09 +0330

    Implemented basic throttler using semaphores, and a test to demonstrate how it works.

---
+++ Appson.Common/Threading.Tests/General/BusyWaitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Appson.Common.Threading.Tests.General
+{
+    internal class BusyWaitTest
+    {
+        public void Run()
+        {
+            var random = new Random();
+            var sha = new SHA256Managed();
+
+            while (true)
+            {
+                var next = new byte[100];
+                random.NextBytes(next);
+                for (int i = 0; i < 200000; i++)
+                {
+                    next = sha.ComputeHash(next);
+                }
+
+                Console.WriteLine(Convert.ToBase64String(next));
+            }
+        }
+    }
+}
---
+++ Appson.Common/Threading.Tests/General/GeneralMenu.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+namespace Appson.Common.Threading.Tests.General
+{
+    internal class GeneralMenu
+    {
+        public void Run()
+        {
+            WriteLine("Please select general scenario:");
+            WriteLine("    1. Busy wait");
+            WriteLine();
+
+            var choice = ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    new BusyWaitTest().Run();
+                    break;
+            }
+        }
+    }
+}
