356d701ec93ebe57524f4196f44a40faed41c1ea
Hamed Iravanchi <iravanchi@gmail.com>
2017-01-22 10:35:14 +0330

    Added ServiceStack dependency to Common.Lucene project

--- Appson.Common/Lucene/IndexManagement/LuceneIndexWrapper.cs
+++ Appson.Common/Lucene/IndexManagement/LuceneIndexWrapper.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using Appson.Common.Lucene.PersianAnalyzer;
 using Appson.Composer;
 using log4net;
 using Lucene.Net.Index;
