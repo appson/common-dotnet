179fc6b44e4ad2f4c9a9864d8126c01a5c625da2
Hamed Iravanchi <iravanchi@gmail.com>
2017-01-22 09:51:23 +0330

    Added Lucene project, initial status (has compile errors)

---
+++ Appson.Common/Lucene/LuceneExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Lucene.Net.Search;
+
+namespace Appson.Common.Lucene
+{
+	public static class LuceneExtensions
+	{
+		public static void AddAll(this BooleanQuery baseQuery, IEnumerable<Query> queries, Occur occur)
+		{
+			queries.ForEach(q => baseQuery.Add(q, occur));
+		}
+	}
+}
---
+++ Appson.Common/Lucene/LuceneIndexHealthStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Appson.Common.Lucene
+{
+    public class LuceneIndexHealthStatus
+	{
+		public string IndexID { get; set; }
+		public bool Initialized { get; set; }
+		public bool ShutDown { get; set; }
+
+		public int NumberOfErrors { get; set; }
+		public DateTime? IndexSearcherLastUsedTimeUtc { get; set; }
+		public DateTime? IndexWriterLastUsedTimeUtc { get; set; }
+		public DateTime? LastCommitTimeUtc { get; set; }
+		public DateTime? LastOptimizationTimeUtc { get; set; }
+
+		public bool HasErrors => NumberOfErrors > 0;
+	}
+}
