ab1ab13a9894824eb57b03453f645d9c93e0ad60
Hamed Iravanchi <iravanchi@gmail.com>
2017-01-25 20:06:57 +0330

    Fixed AssemblyInfo files for release 0.9.1

--- Appson.Common/Stimulsoft/Properties/AssemblyInfo.cs
+++ Appson.Common/Stimulsoft/Properties/AssemblyInfo.cs
@@ -1,36 +1,18 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Stimulstoft")]
+[assembly: AssemblyTitle("Appson.Common.Stimulstoft")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Stimulstoft")]
+[assembly: AssemblyCompany("Appson")]
+[assembly: AssemblyProduct("Appson.Common.Stimulstoft")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("911bfc18-3601-43ca-9d08-3907a4c8fcc4")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.1")]
+[assembly: AssemblyFileVersion("0.9.1")]
