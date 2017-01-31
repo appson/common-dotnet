cfd2b7695ed3585a3b9f7d796523846a70efd607
Hamed Iravanchi <iravanchi@gmail.com>
2017-01-31 20:16:47 +0330

    Add Append extensions to ApiValidationError, and bump version to 0.9.3

--- Appson.Common/General/Validation/ApiValidationResult.cs
+++ Appson.Common/General/Validation/ApiValidationResult.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Appson.Common.General.Validation
@@ -42,5 +43,41 @@ namespace Appson.Common.General.Validation
 		{
 			return new ApiValidationResult {Success = false, Errors = new List<ApiValidationError> {new ApiValidationError(propertyPath, errorKey, errorParameters)}};
 		}
-	}
+
+        public ApiValidationResult Append(ApiValidationError error)
+        {
+            Errors.Add(error);
+            return this;
+        }
+
+        public ApiValidationResult Append(string errorKey)
+        {
+            return Append(new ApiValidationError(errorKey));
+        }
+
+        public ApiValidationResult Append(string errorKey, IEnumerable<string> errorParameters)
+        {
+            return Append(new ApiValidationError(errorKey, errorParameters));
+        }
+
+        public ApiValidationResult Append(string propertyPath, string errorKey)
+        {
+            return Append(new ApiValidationError(propertyPath, errorKey));
+        }
+
+        public ApiValidationResult Append(string propertyPath, string errorKey, IEnumerable<string> errorParameters)
+        {
+            return Append(new ApiValidationError(propertyPath, errorKey, errorParameters));
+        }
+
+        public ApiValidatedResult<T> ToFailedValidatedResult<T>()
+        {
+            if (Success)
+                throw new InvalidOperationException(
+                    "This method should only be called on a ValidationResult instance that contains errors. " +
+                    "Consider checking the state using IsValid property before calling this method.");
+
+            return ApiValidatedResult<T>.Failure(Errors);
+        }
+    }
 }
