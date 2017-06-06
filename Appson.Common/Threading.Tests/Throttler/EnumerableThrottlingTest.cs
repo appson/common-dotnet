edfc27051c920b0cff0ddbca5681d2535d4e9cbb
Kia Panahi Rad <kia.panahirad@gmail.com; kia@appson.ir>
2017-06-06 14:04:56 +0430

    busy waiting. :(

--- Appson.Common/Threading.Tests/Throttler/EnumerableThrottlingTest.cs
+++ Appson.Common/Threading.Tests/Throttler/EnumerableThrottlingTest.cs
@@ -9,13 +9,18 @@ namespace Appson.Common.Threading.Tests.Throttler
     {
         public void Run()
         {
+            WriteLine("Specify the release count per second:");
+            var rps = ReadLine();
+
+            int.TryParse(rps, out int parsedRps);
+
             var second = -1;
             var count = 0;
 
             var lesEnumerables = Enumerable.Range(0, 1000000).ToList();
-            var throttledEnumerables = lesEnumerables.Throttle(50);
+            var throttledEnumerables = lesEnumerables.Throttle(parsedRps);
 
-            foreach (var item in lesEnumerables)
+            foreach (var _ in throttledEnumerables)
             {
                 if (second != DateTime.Now.Second)
                 {
@@ -26,18 +31,7 @@ namespace Appson.Common.Threading.Tests.Throttler
                 }
                 count++;
             }
-
-            foreach (var item in throttledEnumerables)
-            {
-                if (second != DateTime.Now.Second)
-                {
-                    second = DateTime.Now.Second;
-                    WriteLine($" - {count} times");
-                    Write($"{second:D2}: ");
-                    count = 0;
-                }
-                count++;
-            }
+            WriteLine("done (throttled)");
         }
     }
 }
--- Appson.Common/Threading/Extensions/EnumerableExtensions.cs
+++ Appson.Common/Threading/Extensions/EnumerableExtensions.cs
@@ -1,35 +1,37 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Appson.Common.Threading.Extensions
 {
     public static class EnumerableExtensions
     {
-        public static IEnumerable Throttle(this IEnumerable self, int releaseCount)
+        public static IEnumerable<T> Throttle<T>(this IEnumerable<T> self, int releaseCount)
         {
             if (releaseCount == 0)
+            {
                 throw new ArgumentOutOfRangeException(nameof(releaseCount), releaseCount, @"Invalid value provided");
+            }
 
-            var sleepDuration = 1000 / releaseCount;
+            var signaler = new ManualResetEventSlim();
+
+            var sleepDuration = Stopwatch.Frequency / releaseCount;
+
             foreach (var item in self)
             {
-                Thread.Sleep(sleepDuration);
+                Nop(sleepDuration);
                 yield return item;
             }
         }
 
-        public static IEnumerable<T> Throttle<T>(this IEnumerable<T> self, int releaseCount)
+        private static void Nop(long duration)
         {
-            if (releaseCount == 0)
-                throw new ArgumentOutOfRangeException(nameof(releaseCount), releaseCount, @"Invalid value provided");
-
-            var sleepDuration = 1000 / releaseCount;
-            foreach (var item in self)
+            var handle = Stopwatch.StartNew();
+            while (handle.ElapsedTicks < duration)
             {
-                Thread.Sleep(sleepDuration);
-                yield return item;
+                // busy wait! :|
             }
         }
     }
