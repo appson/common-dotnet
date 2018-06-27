2dbcb4303a8f2f5e05b726e02bdfd6256f5c5b04
Hossein Taleghani <hossein.t@appson.ir>
2018-06-27 16:02:43 +0430

    Add AutoLoadItemCache implementation

---
+++ Appson.Common/GeneralComponents/Cache/Components/AutoLoadItemCache.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Appson.Common.GeneralComponents.Cache;
+using Appson.Common.GeneralComponents.Cache.Components;
+using Appson.Composer;
+using Appson.Composer.Cache;
+
+[Component]
+[ComponentCache(typeof(DefaultComponentCache))]
+[IgnoredOnAssemblyRegistration]
+public class AutoLoadItemCache<TKey, TValue> : AutoLoadItemCacheBase<TKey, TValue>, IItemCache<TKey, TValue>
+{
+    [ComponentPlug]
+    public ICacheItemLoader<TKey, TValue> ItemLoader { get; set; }
+
+    public TValue this[TKey key]
+    {
+        get
+        {
+            var creationTimeLimit = DateTime.UtcNow.Ticks - MaximumLifetimeSeconds * 10000000;
+
+            var item = _cacheData.GetOrAdd(key, k => new CacheItem<TValue>(ItemLoader.Load(k)));
+            if (item.LastAccessTime < creationTimeLimit)
+            {
+                CacheItem<TValue> removedValue;
+                _cacheData.TryRemove(key, out removedValue);
+
+                item = _cacheData.GetOrAdd(key, k => new CacheItem<TValue>(ItemLoader.Load(k)));
+            }
+
+            var result = item.Value;
+
+
+            CheckForMaintenance();
+            return Copier.Copy(result);
+        }
+    }
+}
