dada1a3d6dd6f298ec00d505b46042460648055a
Hossein Taleghani <hossein.t@appson.ir>
2018-06-28 14:56:59 +0430

    Add GeneralComponents.Tests project

---
+++ Appson.Common/GeneralComponents.Tests/AutoLoadAsyncItemCacheTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Appson.Common.GeneralComponents.Cache;
+using Appson.Composer;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Appson.Common.GeneralComponents.Tests
+{
+    [TestClass]
+    public class AutoLoadAsyncItemCacheTests
+    {
+        private IAsyncItemCache<string, int> _cache;
+        private ComponentContext _composer;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            
+        }
+
+        [TestMethod]
+        public async Task GetItem_ColdCache_ShouldInvokeLoader()
+        {
+
+        }
+
+        [TestMethod]
+        public async Task GetItem_HotCache_ShouldNotInvokeLoader()
+        {
+
+        }
+
+        [TestMethod]
+        public async Task GetItem_ExpiredHotCache_ShouldInvokeLoader()
+        {
+
+        }
+    }
+}
